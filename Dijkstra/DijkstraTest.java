278e7d7c13ada3aec3a50cf984a277decbbc0ed3
sameesh-s <sameesh.fisat@gmail.com>
2019-10-17 06:16:04 +0530

    Dijkstra Algorithm

---
+++ Dijkstra/DijkstraTest.java
@@ -0,0 +1,13 @@
+
+public class DijkstraTest {
+
+	public static void main(String[] arg){
+		System.out.println("inside the main");
+		
+		int v = 2;
+		//2X2 metrix for adjecency
+		int[][]array = new int[2][2];
+		
+	}
+	
+}
